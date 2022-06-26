87f2933f7f049dd7f5068b9dddec4a58311f8794
Wandile Mawelela <wandile1997@gmail.com>
2022-06-26 07:47:40 +0200

    Added quote file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * main - main block
+  * Return: 1
+  */
+int main(void)
+{
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
+	return (1);
+}
