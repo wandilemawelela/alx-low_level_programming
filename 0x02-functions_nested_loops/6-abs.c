c9fe088cd395ff2f5623e8f103f17122f9eb80fd
Wandile Mawelela <wandile1997@gmail.com>
2022-07-05 13:52:36 +0200

    Added new abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _abs - Main entry point
+  * Description: Computes the absolute value of an integer.
+  * @n: n is an integer
+  * Return: int
+  */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	} else if (n == 0)
+	{
+		return (n);
+	}
+		return (-n);
+}
+
