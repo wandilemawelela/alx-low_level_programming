9722f397052fe1fd2bd9ecef2ad98c0581e37fa6
Wandile Mawelela <wandile1997@gmail.com>
2022-07-05 15:35:53 +0200

    Added new add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - Adds two integers and returns the result.
+  * Description - Adds two integers and returns the result.
+  * @one: First integer
+  * @two: Second integer
+  * Return: sum
+  */
+
+int add(int one, int two)
+{
+	int sum = one + two;
+
+	return (sum);
+}
