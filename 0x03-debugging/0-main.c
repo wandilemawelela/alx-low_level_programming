f457e35b9be1fbd5b92601ce27324c2cd1fdc7dc
Wandile Mawelela <wandile1997@gmail.com>
2022-07-06 12:14:12 +0200

    Hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
-  * positive_or_negative - Assigns i to the variable n.
-  * @i: Interger value
-  */
-
-void positive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = i;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
-}
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -36,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
