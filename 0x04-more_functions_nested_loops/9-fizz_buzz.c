da172c2418b2971187b36c37ff7dbfb5153be866
Wandile Mawelela <wandile1997@gmail.com>
2022-07-07 19:15:10 +0200

    Hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,24 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz\n");
-		} else if ((i % 3) == 0)
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Fizz\n");
-		} else if ((i % 5) == 0)
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Buzz\n");
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
 		} else
 		{
-			printf("%d \n", i);
+			printf(" %d", i);
 		}
 	}
+	printf("\n");
+
+	return (0);
 }
