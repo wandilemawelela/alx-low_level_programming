2e6d1454fda6bf1bed0428370a03e514c12ac27e
Wandile Mawelela <wandile1997@gmail.com>
2022-07-07 19:10:08 +0200

    Added fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+  * Return: 0
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz\n");
+		} else if ((i % 5) == 0)
+		{
+			printf("Buzz\n");
+		} else if (i % 15 == 0)
+		{
+			printf("FizzBuzz\n");
+		} else
+		{
+			printf("%d \n", i);
+		}
+	}
+}
