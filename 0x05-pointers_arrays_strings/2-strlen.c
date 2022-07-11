1c694c1d9730cc41eb87c6e8c40fdadb525df303
Wandile Mawelela <wandile1997@gmail.com>
2022-07-11 08:59:42 +0200

    Added new strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strlen - returns the length of a string
+  * @s: String
+  * Return: Returns the length of the string
+  *
+  */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
