6ab5d0299a64d314b7b0700b6639e6da215766bc
Wandile Mawelela <wandile1997@gmail.com>
2022-07-11 08:44:10 +0200

    Added new swap file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,17 @@
 #include "main.h"
 
+/**
+  * swap_int - swaps the values of two integers.
+  * @a: Interger one
+  * @b: Interger two
+  *
+  */
 
+void swap_int(int *a, int *b)
+{
+	int var;
+
+	var = *a;
+	*a = *b;
+	*b = var;
+}
