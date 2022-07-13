b536b3e4287d45308287c25e1d16fe08e4834c8d
Wandile Mawelela <wandile1997@gmail.com>
2022-07-13 23:03:11 +0200

    Added new to upper file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * *string_toupper - changes all lowercase letters of a string to uppercase
+  * @str: Char to be converted
+  * Return: New string
+  */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
