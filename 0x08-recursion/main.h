276f4daed4c5dd11fbdd0211fec605e0545b9404
Wandile Mawelela <wandile1997@gmail.com>
2022-07-21 20:58:49 +0200

    Added new factorial file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_Hi
 
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 #endif
