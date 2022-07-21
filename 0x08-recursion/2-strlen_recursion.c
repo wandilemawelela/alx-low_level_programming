647513b8224be1bae9d3413853f5cc1daa14158b
Wandile Mawelela <wandile1997@gmail.com>
2022-07-21 20:21:06 +0200

    Added strlen recur file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion -  returns the length of a string.
+  * @s: String
+  * Return: Length of string
+  *
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
