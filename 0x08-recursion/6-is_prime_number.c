b1f2c7b4ae25ffc3cf0ef69a9bc087d038495514
Wandile Mawelela <wandile1997@gmail.com>
2022-07-21 21:38:08 +0200

    Added new is prime file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int check_prime(int, int);
+
+/**
+* is_prime_number -  returns 1 if the input integer is a prime number,
+* otherwise return 0.
+* @n: Number
+* Return:  returns 1 if the input integer is a prime number, otherwise return 0
+*/
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+
+/**
+* check_prime - check prime numbers
+* @n: number
+* @i: iterator
+* Return: a 1 or 0
+*/
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (check_prime(n, i + 1));
+}
