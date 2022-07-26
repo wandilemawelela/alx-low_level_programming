d257fce44a3ba4a2ad5ccac094d2acfe119c56e3
Wandile Mawelela <wandile1997@gmail.com>
2022-07-26 22:43:49 +0200

    Added new str con

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * str_concat - concatenates two strings.
+  * @s1: String one
+  * @s2: String two
+  * Return: NULL
+  *
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[1])
+		i++;
+	while (s2[j])
+		j++;
+
+	k = i + j;
+	s = (char *) malloc(j * sizeof(char) + 1);
+	j = 0;
+	while (l < k)
+	{
+		if (l <= k)
+			s[k] = s1[k];
+		if (l > i)
+		{
+			s[l] = s2[j];
+			j++;
+		}
+		l++;
+	}
+	s[l] = '\0';
+	return (s);
+}
