aae8262a2318281c04eab417af908221f956f6f8
Wandile Mawelela <wandile1997@gmail.com>
2022-08-01 14:52:33 +0200

    Added new dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * File: dog.h
+  * Description: Define a new type struct dog with the following elements:
+  * name, type = char *
+  * age, type = float
+  * owner, type = char *
+  *
+  */
+
+/**
+  * struct dog - description for struct dog
+  * @name: Name of dog
+  * @age: Age of dog
+  * @owner: Owner of dog
+  *
+  *
+  */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
+
