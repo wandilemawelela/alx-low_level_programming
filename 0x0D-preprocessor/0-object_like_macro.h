93499b6caaefd0e28e6b65ff7d5bd1c2017358bf
Wandile Mawelela <wandile1997@gmail.com>
2022-08-01 11:07:40 +0200

    Hot fix

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,10 @@
-#define SIZE 1024
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-#endif
+/**
+  * File: 0-object_like_macro.h
+  * Description: header file that defines a macro named SIZE as an
+  * abbreviation for the token 1024.
+  */
+
+#define SIZE 1024
