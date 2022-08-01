35658646b049a20fa0e27f8e8774904a3f6e5ba7
Wandile Mawelela <wandile1997@gmail.com>
2022-08-01 11:25:09 +0200

    Added new sum file

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+  * File: 4-sum.h
+  * Description:  function-like macro SUM(x, y) that computes the sum of the
+  * numbers x and y.
+  * @x: First number
+  * @y: Second number
+  *
+  */
+
+#define SUM(x, y) (x + y)
+
+#endif
