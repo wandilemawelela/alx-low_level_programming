f5f1a6ee8d1e819df06966a2708672b9a5cd0070
Wandile Mawelela <wandile1997@gmail.com>
2022-08-01 11:10:46 +0200

    Hot fix

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,1 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+  * File: 1-pi.h
+  * Description: header file that defines a macro named PI as an abbreviation
+  * for the token 3.14159265359.
+  *
+  */
+
 #define PI 3.14159265359
+#endif
