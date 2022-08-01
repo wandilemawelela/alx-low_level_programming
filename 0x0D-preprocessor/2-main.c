499031fe606fd605011797a874932aec31ca94d4
Wandile Mawelela <wandile1997@gmail.com>
2022-08-01 10:55:32 +0200

    Added new 2 main file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - prints the name of the file it was compiled from, followed by a
+  * new line.
+  * Return: 0 on success
+  *
+  */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	putchar('\n');
+	return (0);
+}
