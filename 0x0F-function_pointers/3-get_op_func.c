63e6aeb6e75dbc1a258fb23ca0ef6796f185147e
Wandile Mawelela <wandile1997@gmail.com>
2022-08-04 01:05:28 +0200

    Hot fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,31 @@
+#include "3-calc.h"
+#include <stddef.h>
+#include <string.h>
 
+/**
+ * get_op_func - function that returns the function associated with an opcode
+ * @s: operator
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
