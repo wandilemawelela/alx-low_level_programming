68b06b04409dd4ddc4f1cdf7d591038ab1ff6249
Wandile Mawelela <wandile1997@gmail.com>
2022-08-04 21:26:22 +0200

    Hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,79 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
-  *  prints anything.
-  * @format: list of types of arguments passed to the function
-  *
-  */
+ * _printchar - print char type element from va_list
+ * @list: va_list passed to function
+ */
+void _printchar(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
 
+/**
+ * _printstr - print string element from va_list
+ * @list: va_list passed to function
+ */
+void _printstr(va_list list)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
+ * _printfloat - print float type element from va_list
+ * @list: va_list passed to function
+ */
+void _printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * _printint - print int type element from va_list
+ * @list: va_list passed to function
+ */
+void _printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * print_all - print anything passed if char, int, float, or string.
+ * @format: string of formats to use and print
+ */
 void print_all(const char * const format, ...)
 {
-	int i;
-	va_list data;
+	unsigned int i, j;
+	va_list args;
+	char *sep;
 
-	va_start(data, format);
+	checker storage[] = {
+		{ "c", _printchar },
+		{ "f", _printfloat },
+		{ "s", _printstr },
+		{ "i", _printint }
+	};
 
-	for (i = 0; format[i] != '\0'; i++)
+	i = 0;
+	sep = "";
+	va_start(args, format);
+	while (format != NULL && format[i / 4] != '\0')
 	{
-		if (format[i] != '%')
+		j = i % 4;
+		if (storage[j].type[0] == format[i / 4])
 		{
-			printf("%d", format[i]);
+			printf("%s", sep);
+			storage[j].f(args);
+			sep = ", ";
 		}
-		else if (format[i] == '%' && format[i + 1] != ' ')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					printf("%d", va_arg(data, int));
-				break;
-				case 'i':
-					printf("%d", va_arg(data, int));
-				break;
-				case 'f':
-					printf("%d", va_arg(data, int));
-				break;
-				case 's':
-					printf("%s", va_arg(data, char *));
-				break;
-				default:
-				break;
-			}
-			i += 2;
-		}
+		i++;
 	}
-	va_end(data);
+	printf("\n");
+	va_end(args);
 }
