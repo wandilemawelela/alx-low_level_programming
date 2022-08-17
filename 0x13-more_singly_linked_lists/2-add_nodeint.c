5b7bc7a5c8dad9580f1e6129050951827c1ff06f
Wandile Mawelela <wandile1997@gmail.com>
2022-08-17 12:18:54 +0200

    Fixed 2-add_nodeint.c and added tests

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,21 @@
 /**
   * add_nodeint - adds a new node at the beginning of a listint_t list.
   * Return: the address of the new element, or NULL if it failed
-  * @head:
-  * @n:
+  * @head: A pointer to the listint_t list.
+  * @n: Data
   *
   */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node; /* new node */
+
+	new_node = malloc(sizeof(listint_t)); /* allocate memory for the new node */
+
+	if (new_node == NULL) /* if the allocation failed */
+		return (NULL);
+	new_node->n = n; /* assign the data (n) to the new node */
+	new_node->next = *head; /* assign the next node to the new node */
+	*head = new_node; /* assign the new node to the head */
+	return (new_node); /* return the new node */
 }
