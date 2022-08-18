b8950775a76cb1dc28b7d415b547ffaf996dd5e6
Wandile Mawelela <wandile1997@gmail.com>
2022-08-18 19:21:26 +0200

    Added comments

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,7 +13,7 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n >> 1)
-		print_binary(n >> 1);
-	putc((n & 1) ? '1' : '0', stdout);
+	if (n >> 1) /* if n is greater than 1, */
+		print_binary(n >> 1); /* print the binary representation of n / 2 */
+	putc((n & 1) ? '1' : '0', stdout); /* print the binary representation of n % 2 */
 }
