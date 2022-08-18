476e5beeb8e4ffb756f54a100e706592e11a905c
Wandile Mawelela <wandile1997@gmail.com>
2022-08-18 19:20:15 +0200

    Hot fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,14 @@
   * You are not allowed to use arrays
   * You are not allowed to use malloc
   * You are not allowed to use the % or / operators
+  * @n: Number to be converted to binary
   * Return: 0
   *
   */
 
 void print_binary(unsigned long int n)
 {
-	if (n >> 1) {
+	if (n >> 1)
 		print_binary(n >> 1);
-	}
-
 	putc((n & 1) ? '1' : '0', stdout);
 }
