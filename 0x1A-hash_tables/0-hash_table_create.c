b6d02f7567282cbfbcc5e8599d4861a90c6ff03d
Wandile Mawelela <wandile1997@gmail.com>
2023-11-03 20:07:50 +0200

    [FIX] Initialization of hash table memebrs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,21 @@
  *
  */
 
-hash_table_t *hash_table_create()
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 	{
+		return (NULL);
+	}
+
+	table->size = size;
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t*));
+
+	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
 	}
 
