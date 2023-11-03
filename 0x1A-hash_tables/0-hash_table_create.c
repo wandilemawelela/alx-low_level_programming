fed0dc9751b4c64872b08de4fc586c010dc74141
Wandile Mawelela <wandile1997@gmail.com>
2023-11-03 20:09:28 +0200

    [FIX] Betty foo fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
-	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t*));
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
