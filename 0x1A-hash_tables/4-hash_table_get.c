09c330d6e3ee3e96ba421743558ed784f973aa23
Wandile Mawelela <wandile1997@gmail.com>
2023-11-04 20:18:18 +0200

    [ADD] Completed task 4: Write a function that retrieves a value associated with a key.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: The hash table to look into
+ * @key: Key
+ *
+ * Return: Value associated with element, or NULL if key
+ * could not be found.
+ *
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *entry;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	entry = ht->array[index];
+
+	while (entry != NULL)
+	{
+		if (strcmp(entry->key, key) == 0)
+			return (entry->value);
+
+		entry = entry->next;
+	}
+
+	return (NULL);
+}
